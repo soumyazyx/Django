6ddb943d183a5714ad9baea06751af29b58c519b
Soumya Ranjan Das <soumyazyx@gmail.com>
2019-11-30 21:26:21 +0530

    Passing data to templates

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -2,9 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'title1',
+        'content': 'content1',
+        'date_posted': '2019-11-25'
+    },
+    {
+        'author': 'SoumyaRD',
+        'title': 'title2',
+        'content': 'content2',
+        'date_posted': '2019-11-26'
+    },
+]
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
