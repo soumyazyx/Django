2c4d6b976e69ff03cf4d0434319a64db7953c282
Soumya Ranjan Das <soumyazyx@gmail.com>
2020-05-03 08:14:03 +0530

    Removed abandoned TODO app

--- todo/tasks/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from . import views
-urlpatterns = [
-    path('', views.taskshome, name='taskshome'),
-    path('update_task/<str:pk>/', views.updateTask, name='update_task'),
-    path('delete_task/<str:pk>/', views.deleteTask, name='delete_task'),
-]
